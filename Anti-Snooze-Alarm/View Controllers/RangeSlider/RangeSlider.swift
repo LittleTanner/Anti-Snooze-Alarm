a38d758034674ec9036c7feb4b1e6493261ca239
Kevin Tanner <kevindavidtanner@gmail.com>
2019-09-09 13:46:40 -0600

    set up a slider control

---
+++ Anti-Snooze-Alarm/View Controllers/RangeSlider/RangeSlider.swift
@@ -0,0 +1,171 @@
+//
+//  RangeSlider.swift
+//  Anti-Snooze-Alarm
+//
+//  Created by Kevin Tanner on 9/9/19.
+//  Copyright © 2019 Kevin Tanner. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class RangeSlider: UIControl {
+
+    var minimumValue: Double = 0.0 {
+        didSet {
+            updateLayerFrames()
+        }
+    }
+    
+    var maximumValue: Double = 1.0 {
+        didSet {
+            updateLayerFrames()
+        }
+    }
+    
+    var lowerValue: Double = 0.2 {
+        didSet {
+            updateLayerFrames()
+        }
+    }
+    
+    var upperValue: Double = 0.8 {
+        didSet {
+            updateLayerFrames()
+        }
+    }
+    
+    var trackTintColor: UIColor = UIColor(white: 0.9, alpha: 1.0) {
+        didSet {
+            trackLayer.setNeedsDisplay()
+        }
+    }
+    
+    var trackHighlightTintColor: UIColor = UIColor(red: 0.0, green: 0.45, blue: 0.94, alpha: 1.0) {
+        didSet {
+            trackLayer.setNeedsDisplay()
+        }
+    }
+    
+    var thumbTintColor: UIColor = UIColor.white {
+        didSet {
+            lowerThumbLayer.setNeedsDisplay()
+            upperThumbLayer.setNeedsDisplay()
+        }
+    }
+    
+    var curvaceousness: CGFloat = 1.0 {
+        didSet {
+            trackLayer.setNeedsDisplay()
+            lowerThumbLayer.setNeedsDisplay()
+            upperThumbLayer.setNeedsDisplay()
+        }
+    }
+    var previousLocation = CGPoint()
+    
+    let trackLayer = RangeSliderTrackLayer()
+    let lowerThumbLayer = RangeSliderThumbLayer()
+    let upperThumbLayer = RangeSliderThumbLayer()
+    
+    var thumbWidth: CGFloat {
+        return CGFloat(bounds.height)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        trackLayer.rangeSlider = self
+        trackLayer.contentsScale = UIScreen.main.scale
+        layer.addSublayer(trackLayer)
+        
+        lowerThumbLayer.rangeSlider = self
+        lowerThumbLayer.contentsScale = UIScreen.main.scale
+        layer.addSublayer(lowerThumbLayer)
+        
+        upperThumbLayer.rangeSlider = self
+        upperThumbLayer.contentsScale = UIScreen.main.scale
+        layer.addSublayer(upperThumbLayer)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func updateLayerFrames() {
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        
+        trackLayer.frame = bounds.insetBy(dx: 0.0, dy: bounds.height / 3)
+        trackLayer.setNeedsDisplay()
+        
+        let lowerThumbCenter = CGFloat(positionForValue(value: lowerValue))
+        
+        lowerThumbLayer.frame = CGRect(x: lowerThumbCenter - thumbWidth / 2.0, y: 0.0,
+                                       width: thumbWidth, height: thumbWidth)
+        lowerThumbLayer.setNeedsDisplay()
+        
+        let upperThumbCenter = CGFloat(positionForValue(value: upperValue))
+        upperThumbLayer.frame = CGRect(x: upperThumbCenter - thumbWidth / 2.0, y: 0.0,
+                                       width: thumbWidth, height: thumbWidth)
+        upperThumbLayer.setNeedsDisplay()
+        
+        CATransaction.commit()
+    }
+    
+    func positionForValue(value: Double) -> Double {
+        return Double(bounds.width - thumbWidth) * (value - minimumValue) /
+            (maximumValue - minimumValue) + Double(thumbWidth / 2.0)
+    }
+    
+    override var frame: CGRect {
+        didSet {
+            updateLayerFrames()
+        }
+    }
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        previousLocation = touch.location(in: self)
+        
+        // Hit test the thumb layers
+        if lowerThumbLayer.frame.contains(previousLocation) {
+            lowerThumbLayer.highlighted = true
+        } else if upperThumbLayer.frame.contains(previousLocation) {
+            upperThumbLayer.highlighted = true
+        }
+        
+        return lowerThumbLayer.highlighted || upperThumbLayer.highlighted
+    }
+    
+    func boundValue(value: Double, toLowerValue lowerValue: Double, upperValue: Double) -> Double {
+        return min(max(value, lowerValue), upperValue)
+    }
+    
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let location = touch.location(in: self)
+        
+        // 1. Determine by how much the user has dragged
+        let deltaLocation = Double(location.x - previousLocation.x)
+        let deltaValue = (maximumValue - minimumValue) * deltaLocation / Double(bounds.width - thumbWidth)
+        
+        previousLocation = location
+        
+        // 2. Update the values
+        if lowerThumbLayer.highlighted {
+            lowerValue += deltaValue
+            lowerValue = boundValue(value: lowerValue, toLowerValue: minimumValue, upperValue: upperValue)
+        } else if upperThumbLayer.highlighted {
+            upperValue += deltaValue
+            upperValue = boundValue(value: upperValue, toLowerValue: lowerValue, upperValue: maximumValue)
+        }
+        
+        sendActions(for: .valueChanged)
+        
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        lowerThumbLayer.highlighted = false
+        upperThumbLayer.highlighted = false
+    }
+
+}
---
+++ Anti-Snooze-Alarm/View Controllers/RangeSlider/RangeSliderTrackLayer.swift
@@ -0,0 +1,35 @@
+//
+//  RangeSliderTrackLayer.swift
+//  Anti-Snooze-Alarm
+//
+//  Created by Kevin Tanner on 9/9/19.
+//  Copyright © 2019 Kevin Tanner. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class RangeSliderTrackLayer: CALayer {
+    weak var rangeSlider: RangeSlider?
+    
+    override func draw(in ctx: CGContext) {
+        if let slider = rangeSlider {
+            // Clip
+            let cornerRadius = bounds.height * slider.curvaceousness / 2.0
+            let path = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
+            ctx.addPath(path.cgPath)
+            
+            // Fill the track
+            ctx.setFillColor(slider.trackTintColor.cgColor)
+            ctx.addPath(path.cgPath)
+            ctx.fillPath()
+            
+            // Fill the highlighted range
+            ctx.setFillColor(slider.trackHighlightTintColor.cgColor)
+            let lowerValuePosition = CGFloat(slider.positionForValue(value: slider.lowerValue))
+            let upperValuePosition = CGFloat(slider.positionForValue(value: slider.upperValue))
+            let rect = CGRect(x: lowerValuePosition, y: 0.0, width: upperValuePosition - lowerValuePosition, height: bounds.height)
+            ctx.fill(rect)
+        }
+    }
+}
