a5af02a8c4f42a6aee68f77e549e096dbba9e9c0
Kevin Tanner <kevindavidtanner@gmail.com>
2019-09-10 14:26:05 -0600

    created an alert controller for when the alarm goes off

---
+++ Anti-Snooze-Alarm/View Controllers/MainViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MainViewController.swift
+//  Anti-Snooze-Alarm
+//
+//  Created by Kevin Tanner on 9/10/19.
+//  Copyright © 2019 Kevin Tanner. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func presentAlertControllerButtonTapped(_ sender: Any) {
+        presentAlarmAlert()
+    }
+    
+    func presentAlarmAlert() {
+        // Create alert controller
+        let alertController = UIAlertController(title: "It's Time To Wake Up!", message: "When ready click start below to get started", preferredStyle: .alert)
+        
+        // Create action
+        let startGame = UIAlertAction(title: "START", style: .default) { (_) in
+            print("User tapped the start button, send them to a mini game")
+        }
+        
+        // Add action
+        alertController.addAction(startGame)
+        
+        // Present alert controller
+        self.present(alertController, animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
