b2906dbde65f675836c5991429e9b0e5f6d9b8aa
Kevin Tanner <kevindavidtanner@gmail.com>
2019-09-19 19:18:17 -0600

    creates a working prototype for the wordOfTheDay game but need to figure out how to make it more user friendly because the API call takes a while

---
+++ Anti-Snooze-Alarm/Model Controllers/RandomWordController.swift
@@ -0,0 +1,78 @@
+//
+//  RandomWordController.swift
+//  Anti-Snooze-Alarm
+//
+//  Created by Kevin Tanner on 9/19/19.
+//  Copyright © 2019 Kevin Tanner. All rights reserved.
+//
+
+import Foundation
+
+class RandomWordController {
+    
+    // URL to create
+    // https://random-word-api.herokuapp.com/word?key=WMDFHP4S&number=1
+    
+    func fetchRandomWord(completion: @escaping (String?) -> Void) {
+        
+        guard let baseURL = URL(string: "https://random-word-api.herokuapp.com/word") else {
+            completion(nil)
+            return
+        }
+        
+        guard var components = URLComponents(url: baseURL, resolvingAgainstBaseURL: true) else {
+            print("Our URL is not working right. In function: \(#function)")
+            completion(nil)
+            return
+        }
+        
+        let apiKey = "WMDFHP4S"
+        
+        let apiKeyQueryItem = URLQueryItem(name: "key", value: apiKey)
+        let numberOfWordsReturnedQueryItem = URLQueryItem(name: "number", value: "1")
+        
+        components.queryItems = [apiKeyQueryItem, numberOfWordsReturnedQueryItem]
+        
+        guard let builtURL = components.url else {
+            print("Our query items are causing trouble in function: \(#function)")
+            completion(nil)
+            return
+        }
+        
+        
+        print("The builtURL is: \(builtURL)")
+        
+        let dataTask = URLSession.shared.dataTask(with: builtURL) { (data, response, error) in
+            if let error = error {
+                print("There was an error with the url in \(#function). \n Error: \(error), \n Error Localized Description: \(error.localizedDescription)")
+                completion(nil)
+                return
+            }
+            
+            //            if let response = response {
+            //                print("The response from Weather was: \(response)")
+            //            }
+            
+            guard let data = data else {
+                print("There was no random word data found in \(#function).")
+                completion(nil)
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                let randomWordObject = try jsonDecoder.decode([String].self, from: data)
+                if let randomWord = randomWordObject.first {
+                    completion(randomWord)
+                }
+            } catch {
+                print("There was an error decoding random word JSON in \(#function). \n Error: \(error), \n Error Localized Description: \(error.localizedDescription)")
+                completion(nil)
+                return
+            }
+        }
+        dataTask.resume()
+    }
+    
+}
--- Anti-Snooze-Alarm/View Controllers/Mini Games/WordOfTheDayViewController.swift
+++ Anti-Snooze-Alarm/View Controllers/Mini Games/WordOfTheDayViewController.swift
@@ -12,8 +12,16 @@ class WordOfTheDayViewController: UIViewController {
 
     // MARK: - Outlets
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var instructionsLabel: UILabel!
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var wordDefinitionLabel: UILabel!
+    @IBOutlet weak var inputDefinitionTextField: UITextView!
+    
     
     // MARK: - Properties
+    
+    var randomWord = ""
     var word = ""
     var definition = ""
     
@@ -21,40 +29,110 @@ class WordOfTheDayViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.view.backgroundColor = UIColor.darkBlue
+        inputDefinitionTextField.becomeFirstResponder()
         setsUpUI()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        let wordController = WordController()
-        
-        wordController.fetchWordOfTheDay(word: "test") { (word) in
-            guard let word = word,
-                let searchedWord = word.word,
-                let definition = word.definition else { return }
+        let randomWordController = RandomWordController()
+        randomWordController.fetchRandomWord { (randomWord) in
             
-            self.word = searchedWord
-            self.definition = definition
+            guard let randomWord = randomWord else { return }
             
-            print("Searched Word: \(searchedWord)")
-            print("Definition: \(definition)")
+            self.randomWord = randomWord
+            print(randomWord)
+            
+            let wordController = WordController()
+            
+            wordController.fetchWordOfTheDay(word: randomWord) { (word) in
+                guard let word = word,
+                    let searchedWord = word.word,
+                    let definition = word.definition else { return }
+                
+                self.word = searchedWord
+                let definitionWithTab = definition.components(separatedBy: "\t")
+                
+                let definitionRandomizedCases = definitionWithTab[1].map {
+                    if Int.random(in: 0...1) == 0 {
+                        return String($0).lowercased()
+                    }
+                    return String($0).uppercased()
+                    }.joined(separator: "")
+                
+                self.definition = definitionRandomizedCases
+                
+                print("Searched Word: \(searchedWord)")
+                print("Definition: \(definition)")
+                
+                self.setsUpUI()
+            }
         }
+        
+//        let wordController = WordController()
+//
+//        wordController.fetchWordOfTheDay(word: "test") { (word) in
+//            guard let word = word,
+//                let searchedWord = word.word,
+//                let definition = word.definition else { return }
+//
+//            self.word = searchedWord
+//            let definitionWithTab = definition.components(separatedBy: "\t")
+//
+//            let definitionRandomizedCases = definitionWithTab[1].map {
+//                if Int.random(in: 0...1) == 0 {
+//                    return String($0).lowercased()
+//                }
+//                return String($0).uppercased()
+//                }.joined(separator: "")
+//
+//            self.definition = definitionRandomizedCases
+//
+//            print("Searched Word: \(searchedWord)")
+//            print("Definition: \(definition)")
+//        }
     }
     
     // MARK: - Actions
     
+    @IBAction func skipButtonTapped(_ sender: Any) {
+        // generate random word, fetch random word, update UI
+    }
     
-    // MARK: - Custom Methods
-    
-    func setsUpUI() {
-        self.view.backgroundColor = UIColor.darkBlue
-        DispatchQueue.main.async {
-            // labels for word and definition updated
+    @IBAction func enterButtonTapped(_ sender: Any) {
+        guard let inputText = inputDefinitionTextField.text else { return }
+        
+        if definition == inputText {
+            print("Correct")
+            // Create an instance of the main storyboard
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            // Create an instance of the view controller
+            let controller = storyboard.instantiateViewController(withIdentifier: "mainNavigationController")
+            // Present the user with the main view controller
+            self.present(controller, animated: true, completion: nil)
+        } else {
+            print("Incorrect")
         }
     }
     
+    
+    // MARK: - Custom Methods
+    
+    
+    // MARK: - UI Adjustments
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    func setsUpUI() {
+        DispatchQueue.main.async {
+            self.wordLabel.text = self.word
+            self.wordDefinitionLabel.text = self.definition
+        }
+    }
     
     /*
      // MARK: - Navigation
